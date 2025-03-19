09a9b5713edd807be896f599ec940da5df737ad9
Thomas Bitonti <bitonti@us.ibm.com>
2025-03-19 17:08:21 -0400

    Prototype

--- dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/app/deploy/extended/ApplicationInfoForContainer.java
+++ dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/app/deploy/extended/ApplicationInfoForContainer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018,2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -21,7 +21,6 @@ package com.ibm.ws.container.service.app.deploy.extended;
  * this interface.
  */
 public interface ApplicationInfoForContainer {
-
     boolean getUseJandex();
-
+    String getAnnotationScanLibrary();
 }
